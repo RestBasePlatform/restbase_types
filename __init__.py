abd37be42424ff41bf140a33b401a972988bfb35
mv-yurchenko <maxonus2@gmail.com>
2021-09-06 21:19:40 +0300

    Initial data types

---
+++ __init__.py
@@ -0,0 +1,12 @@
+from .database_table_info import Database
+from .database_table_info import DatabaseColumn
+from .database_table_info import DatabaseSchema
+from .database_table_info import DatabaseTable
+
+
+__all__ = [
+    "DatabaseColumn",
+    "DatabaseTable",
+    "DatabaseSchema",
+    "Database",
+]
---
+++ database_table_info.py
@@ -0,0 +1,46 @@
+from typing import List
+
+
+class DatabaseColumn:
+    def __init__(self, name: str, data_type):
+        self.name = name
+        self.data_type = data_type
+
+    name: str
+    data_type: str
+
+
+class DatabaseSchema:
+    name: str
+
+    def __init__(self, name: str):
+        self.name = name
+
+
+class Database:
+    name: str
+
+    def __init__(self, name: str):
+        self.name = name
+
+
+class DatabaseTable:
+    def __init__(
+        self,
+        name: str,
+        schema: DatabaseSchema,
+        database: Database,
+        size_mb: float,
+        column_list: List[DatabaseColumn],
+    ):
+        self.name = name
+        self.schema = schema
+        self.database = database
+        self.size_mb = size_mb
+        self.column_list = column_list
+
+    database: Database
+    schema: DatabaseSchema
+    name: str
+    size_mb: float
+    column_list: List[DatabaseColumn]
